66deaae69b4301229f935f165f6844c1f6a312b2
egecue <egecelikci@gmail.com>
2019-06-23 04:41:50 +0300

    Deleted "purge.ts" and merged codes of it with "purge.ts"
    Both of them is fully functional now.

--- src/commands/Moderation/prune.ts
+++ src/commands/Moderation/prune.ts
@@ -1,5 +1,4 @@
-import { Command, CommandStore, KlasaMessage, CommandOptions } from "klasa";
-import { GuildMember } from 'discord.js';
+import { Command, CommandStore, KlasaMessage, CommandOptions, KlasaUser } from "klasa";
 import { SudoClient } from "../../core/SudoClient";
 
 export default class extends Command {
@@ -9,30 +8,42 @@ export default class extends Command {
             enabled: true,
             permissionLevel: 1,
             requiredPermissions: ['MANAGE_MESSAGES'],
-            aliases: [],
-            usage: "[amount:int]"
+            aliases: ["purge", "clear", "clean"],
+            description: language => language.get("COMMAND_PRUNE_DESCRIPTION"),
+            runIn: ['text'],
+            usage: "<amount:int> [user:user]",
+            usageDelim: " "
         });
     }
 
-    async run(msg: KlasaMessage, [amount, member]: [number, GuildMember]): Promise<KlasaMessage | KlasaMessage[]> {
-        if (!amount || amount < 1) throw msg.reply(msg.language.get("AMOUNT_FALSE"));
-        if (!member) throw msg.reply(msg.language.get("MENTION_MEMBER"));
-        
-        const messages: Message[] = (await msg.channel.messages.fetch({
+    async run(msg: KlasaMessage, [amount, user]: [number, KlasaUser]): Promise<any> {
+        if (user) {
+            let messages = (await msg.channel.messages.fetch({
                 limit: 100,
                 before: msg.id
             }))
-            .filter((a: Message) => a.author.id === member.id)
+            .filter((a: KlasaMessage) => a.author.id === user.id)
             .array();
-        messages.length = Math.min(quantity, messages.length);
-        
-        if (messages.length === 0) return msg.reply(msg.language.get("NO_MESSAGES_FROM_THAT_USER");
-        if (messages.length === 1) await messages[0].delete();
-        else await msg.channel.bulkDelete(messages);
-        if (member.id === msg.author.id) msg.delete();
-
-        throw msg.reply('👌').then((sentMsg: KlasaMessage) => sentMsg.delete({
-            timeout: 3000
-        }));
+            messages.length = Math.min(amount, messages.length);
+            if (user.id === msg.author.id) msg.delete();
+            if (messages.length === 0) throw msg.reply(msg.language.get("NO_MESSAGE_FROM_THAT_USER"));
+            if (messages.length === 1) await messages[0].delete();
+            else await msg.channel.bulkDelete(messages).then(() => {
+                throw msg.reply("👌").then((sentMsg: KlasaMessage) => sentMsg.delete({
+                    timeout: 3000
+                }));
+            });;
+        } else {
+            let messages = (await msg.channel.messages.fetch({
+                limit: Math.min(amount, 100),
+                before: msg.id
+            })); 
+            msg.delete();
+            await msg.channel.bulkDelete(messages).then(() => {
+                throw msg.reply("👌").then((sentMsg: KlasaMessage) => sentMsg.delete({
+                    timeout: 3000
+                }));
+            });
+        };
     }
 }
