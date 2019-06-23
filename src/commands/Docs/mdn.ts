8ed0faaba6b7fe41f300888bb746919775cc1dcc
egecue <egecelikci@gmail.com>
2019-06-23 04:46:38 +0300

    Created "Docs/mdn.ts" command and added/upgraded some packages.

---
+++ src/commands/Docs/mdn.ts
@@ -0,0 +1,46 @@
+import { Command, CommandStore, KlasaMessage, CommandOptions } from "klasa";
+import { SudoClient } from "../../core/SudoClient";
+import { MessageEmbed } from "discord.js";
+import { get } from "request";
+import { stringify } from "query-string";
+import * as Turndown from "turndown";
+
+export default class extends Command {
+    constructor(client: SudoClient, store: CommandStore, file, dir, options?: CommandOptions) {
+        super(client, store, file, dir, {
+            name: "mdn",
+            enabled: true,
+            requiredPermissions: ['EMBED_LINKS'],
+            aliases: ["mozilla-developer-network"],
+            description: language => language.get("COMMAND_MDN_DESCRIPTION"),
+            usage: "[query:string]",
+            usageDelim: " "
+        });
+    }
+
+    async run(msg: KlasaMessage, [query, match]: [string, any]): Promise<any> {
+        
+        const turndown = new Turndown();
+        if (!query && match) query = match[1];
+
+        await get(`https://mdn.pleb.xyz/search?${stringify({ q: query })}`, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                body = JSON.parse(body);
+                if (!body.URL || !body.Title || !body.Summary) {
+                    throw msg.reply(msg.language.get("ERROR"));
+                };
+                turndown.addRule('hyperlink', {
+                    filter: 'a',
+                    replacement: (text, node) => `[${text}](https://developer.mozilla.org${node.href})`
+                });
+                const embed = new MessageEmbed()
+                    .setColor(0x066FAD)
+                    .setAuthor('MDN', 'https://wiki.mozilla.org/images/1/1b/Mdn_logo_color.png', 'https://developer.mozilla.org/')
+                    .setURL(`https://developer.mozilla.org${body.URL}`)
+                    .setTitle(body.Title)
+                    .setDescription(turndown.turndown(body.Summary.replace(/<code><strong>(.+)<\/strong><\/code>/g, '<strong><code>$1<\/code><\/strong>')));
+                    return msg.channel.send(embed);
+            };
+        });
+    }
+}
